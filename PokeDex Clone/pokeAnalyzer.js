3e62dd4d26401761dc59dca667a5da16d98ec12b
Redl3lood <raymond4808@gmail.com>
2024-05-09 12:00:16 -0500

    added search bar and CSS for elements of pokemon

--- PokeDex Clone/pokeAnalyzer.js
+++ PokeDex Clone/pokeAnalyzer.js
@@ -3,11 +3,13 @@
 // git add .
 // git commit -m "Commit message"
 // git push
+const searchInput= document.getElementById("search");
+searchInput.addEventListener("input", filterSearch)
 
-const pokeLoadCount = 25; //temp loading number
+const pokeLoadCount = 151; //loading number for gen 1 pokemon
 var pokeDex= {};
 
-window.onload = async function(){ //onload loops through max pokeLoadCount to populate and load pokemon to display at the bottom
+async function loadDisplay(){ //onload loops through max pokeLoadCount to populate and load pokemon to display at the bottom
     for (let i =1; i <= pokeLoadCount; i++){
         //waits for pokiInfo to load before creating HTML elements
         await loadPokeApi(i); //test with ID1
@@ -27,8 +29,10 @@ window.onload = async function(){ //onload loops through max pokeLoadCount to po
         
         //<div id="1" class="pokeListName"> <img class="pokeListThumbnail" src="API URL"> </div>
     }
-    console.log(pokeDex); //troubleshooting purposes
+    //console.log(pokeDex); //troubleshooting purposes
 }
+
+window.onload = loadDisplay();
 
 async function loadPokeApi(input){ //async function that loads pokeInfo pulled from pokeAPI to populate pokeDex object array to be used later
     let url = "https://pokeapi.co/api/v2/pokemon/" + input.toString()
@@ -61,8 +65,8 @@ function updatePokeFunction (){ //update poke function clears existing info and 
     document.getElementById("pokeImg").src = pokeDex[this.id]["img"];
 
     document.getElementById("pokeName").innerHTML= pokeDex[this.id]["name"].charAt(0).toUpperCase()+pokeDex[this.id]["name"].slice(1);
-
-    document.getElementById("pokeDesc").innerHTML= pokeDex[this.id]["desc"]["flavor_text_entries"][8]["flavor_text"]
+    
+    document.getElementById("pokeDesc").innerHTML= pokeDex[this.id]["desc"]["flavor_text_entries"][9]["flavor_text"].replace('\f','\n'); //.replace removes the random arrows in some pokemon descriptions
     document.getElementById("pokeDesc").classList.add("pokeDesc");
 
     let typesDiv = document.getElementById("pokeType");
@@ -72,12 +76,57 @@ function updatePokeFunction (){ //update poke function clears existing info and 
     for (let i = 0; i < updatedTypes.length; i++){
         let newTypeDiv= document.createElement("span");
         newTypeDiv.classList.add("type-holder"); //adds class so we can CSS adjust to each class later
-        newTypeDiv.classList.add(updatedTypes[0]["type"]["name"]);//adds class so we can CSS adjust to each class later
+        newTypeDiv.classList.add(updatedTypes[i]["type"]["name"]);//adds class so we can CSS adjust to each class later
     
         newTypeDiv.innerText= updatedTypes[i]["type"]["name"].toUpperCase();
         typesDiv.appendChild(newTypeDiv);
 
     }
 
-    //Work in progress: weakness and strengths w/ color, background color update on click
+    //Work in progress: weakness and strengths w/ color
 }
+
+async function loadDisplayInput(input){ //onload loops through max pokeLoadCount to populate and load pokemon to display at the bottom and filters out based on input
+    for (let i =1; i <= pokeLoadCount; i++){
+        //waits for pokiInfo to load before creating HTML elements
+        await loadPokeApi(i);
+        console.log(input);
+        //let cleanedInput= input.toLowerCase();
+
+        if (pokeDex[i]["name"].match(input.toLowerCase())){
+            let pokemon = document.createElement("div");
+            pokemon.id=i;
+            pokemon.innerText=i.toString()+". "+ pokeDex[i]["name"].charAt(0).toUpperCase()+pokeDex[i]["name"].slice(1);
+            
+            pokemon.classList.add("pokeListBox");
+            pokemon.addEventListener("click", updatePokeFunction); //work in progress (below) adds event listener to detect clicks to update the upper half with relative poke info
+            document.getElementById("displayList").append(pokemon);
+    
+            let img = document.createElement("img");
+            img.src= pokeDex[i]["img"];
+            img.classList.add("pokeListThumbnail");
+            pokemon.appendChild(img);
+            
+            //<div id="1" class="pokeListName"> <img class="pokeListThumbnail" src="API URL"> </div>
+            
+        }      
+    }
+}
+
+
+function filterSearch(){
+    input= document.getElementById("search").value;
+    console.log(input);
+
+    if (document.getElementById("search").innerHTML=""){
+        loadDisplay();
+    }
+    
+    else{
+        document.getElementById("displayList").innerHTML="";
+        loadDisplayInput(input)
+    }
+
+
+}
+
